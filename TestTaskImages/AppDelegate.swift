513cf038189461f071ce546159d466a5ecbdfcb2
Natalia_Markina <natalia.marckina2010@yandex.ru>
2022-04-29 11:15:35 +0300

    Create project

---
+++ TestTaskImages/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TestTaskImages
+//
+//  Created by Natalia on 29.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = MainViewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ TestTaskImages/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskImages
+//
+//  Created by Natalia on 29.04.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
